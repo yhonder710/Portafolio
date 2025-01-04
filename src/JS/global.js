dd50d9f57a8b621648efbdb65d9c95f3246fd72b
Kpo710 <agueropibe1@gmail.com>
2025-01-04 16:15:04 -0400

    pagina principal lista 4.0

--- src/JS/global.js
+++ src/JS/global.js
@@ -1,13 +1,24 @@
 const btnBars = document.querySelector('.btn-bars');
 const menuResponsive = document.querySelector('.menu-responsive');
 const btnClose = document.querySelector('.btn-close');
+const main = document.querySelector('.main');
+const redesSociales = document.querySelector('.redes-sociales');
 
 btnBars.addEventListener('click', () => {
   menuResponsive.classList.toggle('active');
   document.body.classList.toggle('no-scroll');
+  if (menuResponsive.classList.contains('active')) {
+    main.classList.toggle('hiden');
+    redesSociales.classList.toggle('hiden');
+
+  }
 });
 
 btnClose.addEventListener('click', () => {
   menuResponsive.classList.remove('active');
   document.body.classList.remove('no-scroll');
+  if (!menuResponsive.classList.contains('active')) {
+    main.classList.remove('hiden');
+    redesSociales.classList.remove('hiden');
+  }
 });
